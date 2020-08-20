e9dd49013b033f992ed35a8a51270b9aea617a86
Minion0608 <F16CS180@gmail.com>
2020-08-21 00:24:55 +0500

    first line

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+// var num = 3.6;
+// var round = Math.round(num);
+// console.log(round)
+
+
+// ///////APPLICATON OF TOSS 
+var headUser = prompt("Enter heads username");
+var tailUser = prompt("Enter tails username");
+ var toss = Math.round() * 2;
+ var floor = Math.floor(toss)
+ if (floor === 0) {
+ 	alert("HEADS " + headUser + "win the toss");
+ }
+ else {
+ 	alert("TIALS " + tailUser + " win the toss");
+ }
